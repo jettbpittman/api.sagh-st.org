0b1891c48a5246f49a3d2d33fe14ec9fc65ef043
Jett B Pittman <jett@jettpittman.com>
2023-03-18 23:15:03 -0500

    change password

--- main.py
+++ main.py
@@ -767,7 +767,7 @@ async def change_password(request: web.Request) -> web.Response:
     if a.status != 200:
         return a
     req_id = request.match_info['id']
-    print(a)
+    print(a.body)
     if a['id'] != req_id:
         return web.json_response({"status": "failed", "reason": "forbidden"})
     info = await request.json()
