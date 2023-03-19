64c6bd63afde514fb704f3fa28be60a3567537c6
Jett B Pittman <jett@jettpittman.com>
2023-03-18 23:13:53 -0500

    change password

--- main.py
+++ main.py
@@ -762,7 +762,6 @@ async def corsSucks(request: web.Request) -> web.Response:
 
 
 @router.post("/users/{id}/password")
-@handle_json_error
 async def change_password(request: web.Request) -> web.Response:
     a = await auth_required(request, permissions=0)
     if a.status != 200:
