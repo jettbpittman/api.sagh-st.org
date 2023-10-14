e6fdd3594919707e806ca39034741af4650b1998
Jett B Pittman <jett@jettpittman.com>
2023-10-13 20:34:48 -0500

    add attendance submit

--- main.py
+++ main.py
@@ -783,11 +783,10 @@ async def submit_attendance(request: web.Request) -> web.Response:
     info = await request.json()
     payload: dict = info
     date = payload.pop('date')
-    resp = {}
-    resp += {"date": date}
+    resp = {"date": date}
     for swimmer in payload:
         await db.execute("INSERT INTO attendance (date, swimmer, status) VALUES ($1, $2, $3)", date, swimmer, payload[swimmer])
-        resp += {swimmer: payload[swimmer]}
+        resp[swimmer] = payload[swimmer]
     return web.json_response(resp)
 
 
