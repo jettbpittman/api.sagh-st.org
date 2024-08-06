30e7b47a2b2bb7652bbc04a380e0f93b8da54c08
Jett B Pittman <jett@jettpittman.com>
2024-08-06 14:45:34 -0500

    add close meets

--- main.py
+++ main.py
@@ -1773,7 +1773,7 @@ async def get_meets_within_two_weeks(request: web.Request) -> web.Response:
     dwb = f"{dateweekbefore.year}{dateweekbefore.month}{dateweekbefore.day}"
     dateweeklater = datenow + datetime.timedelta(days=7)
     dwl = f"{dateweeklater.year}{dateweeklater.month}{dateweeklater.day}"
-    meets = await db.fetch(f"SELECT * FROM meets where startdate BETWEEN {dwb} AND {dwl}")
+    meets = await db.fetch(f"SELECT * FROM meets where startdate BETWEEN {str(dwb)} AND {str(dwl)}")
     html = ""
     for meet in meets:
         times = ""
