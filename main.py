cac6aa73751eabc545bbab9ae05a56711b7e14b3
Jett B Pittman <jett@jettpittman.com>
2024-10-05 10:42:31 -0500

    fix meet display on front page

--- main.py
+++ main.py
@@ -2014,7 +2014,7 @@ async def get_latest_meet(request: web.Request) -> web.Response:
 async def get_meets_within_two_weeks(request: web.Request) -> web.Response:
     db = request.config_dict["DB"]
     datenow = datetime.datetime.now()
-    dateweekbefore = datenow - datetime.timedelta(days=7)
+    dateweekbefore = datenow - datetime.timedelta(days=14)
     dwb = f"{dateweekbefore.year}{dateweekbefore.month:02d}{dateweekbefore.day:02d}"
     dateweeklater = datenow + datetime.timedelta(days=7)
     dwl = f"{dateweeklater.year}{dateweeklater.month:02d}{dateweeklater.day:02d}"
