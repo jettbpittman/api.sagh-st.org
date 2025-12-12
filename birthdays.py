6a20f56c8325de42b65978cb2a671fe5a4c46208
Jett Pittman <jettpittman@utexas.edu>
2025-12-12 16:02:44 -0600

    fix birthdfays

--- birthdays.py
+++ birthdays.py
@@ -26,7 +26,10 @@ for swimmer in swimmers:
     print(swimmer)
     if swimmer[11] is None:
         continue
-    birthday = datetime.strptime(swimmer[11], '%Y-%m-%d')
+    try:
+        birthday = datetime.strptime(swimmer[11], '%Y-%m-%d')
+    except ValueError:
+        break
     age_dt = today - birthday
     age = int(age_dt.days / 365)
     print(age)
