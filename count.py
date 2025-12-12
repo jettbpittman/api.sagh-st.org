fd31ccd5bdefb309a7a4d83ccf857c00432e17cd
Jett Pittman <jettpittman@utexas.edu>
2025-12-12 15:55:29 -0600

    add better count and sasa

--- count.py
+++ count.py
@@ -1,5 +1,6 @@
 import psycopg2
 import json
+import tabulate
 
 with open("creds.json", "r") as f:
     creds = json.load(f)
@@ -15,36 +16,22 @@ con = psycopg2.connect(
 
 cur = con.cursor()
 
-c = cur.execute("""SELECT * FROM swimmers""")
+c = cur.execute("""SELECT id, first_name, middle_name, last_name FROM swimmers WHERE id not in (1, 2, 3)""")
 swimmers = cur.fetchall()
 
-count = dict()
+count = []
 
 for swimmer in swimmers:
     s = cur.execute(f"SELECT count(*) FROM entries WHERE swimmer = {swimmer[0]} AND seed != 'RL'")
     l = cur.fetchall()
+    r = cur.execute(f"SELECT count(*) FROM relays WHERE {swimmer[0]} in (swimmer_1, swimmer_2, swimmer_3, swimmer_4)")
+    j = cur.fetchall()
     name = f"{swimmer[3]}, {swimmer[1]} {swimmer[2]}".strip()
-    count.update({name: l[0][0]})
+    count.append([l[0][0] + j[0][0], name])
 
-r = cur.execute("""SELECT * FROM relays""")
-relays = cur.fetchall()
+count.sort(key=lambda x: x[0], reverse=True)
 
-c = cur.execute("""SELECT""")
-
-for relay in relays:
-    counter = 1
-    while counter < 5:
-        cur.execute(f"SELECT first_name, middle_name, last_name FROM swimmers WHERE id = {relay[counter]}")
-        l = cur.fetchall()
-        name = f"{l[0][2]}, {l[0][0]} {l[0][1]}".strip()
-        count[name] += 1
-        counter += 1
-
-rcount = dict(sorted(count.items(), key=lambda item: item[1], reverse=True))
-
-for line in rcount:
-    print(f"{line} - {rcount[line]}")
-
+print(tabulate.tabulate(count, headers=["SWIMS", "NAME"], tablefmt='orgtbl'))
 #sorted = sorted(count.items(), key=lambda x: x[1], reverse=True)
 #count = dict(sorted)
 #for swimmer in count:
